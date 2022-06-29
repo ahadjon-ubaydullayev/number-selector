5b328949f09748cc879e8be877e7c1fdfd903b1d
Ahadjon Ubaydullayev <ahadjon.ubaydullayev16@gmail.com>
2022-06-28 23:02:53 -0400

    first commit

--- number_selection/settings.py
+++ number_selection/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-w3mh2uhs9z!$@3vbd&-^d8)q*-5zt!#)puei&&d&sb38pdbef#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['number-selector.herokuapp.com', 'ed13-95-214-210-201.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['number-selector.herokuapp.com', '9267-213-230-99-218.ngrok.io', '127.0.0.1']
 
  
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'number_selection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'number_selection.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
